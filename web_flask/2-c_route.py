5603be2fba4639efe7428c66cf33a4dc24886b2e
ChongLeangUENG <leangcnrp777@gmail.com>
2022-11-28 07:46:45 +0100

    C Route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,20 +11,24 @@ def hbnb():
     """web application must be listening on 0.0.0.0, port 5000"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """print HBNB"""
     return "HBNB"
+
 
 @app.route('/c/hbnb', strict_slashes=False)
 def hbnb1():
     """print HBNB"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def hbnb2(text):
     """print c + text"""
-    return "C {}".format(text.replace("_", " "))
+    return 'C ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)  # host="0.0.0.0"
