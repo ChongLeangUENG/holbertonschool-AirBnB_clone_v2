fbc588bfe4f1222b51f99d78b52948e409803a96
ChongLeangUENG <leangcnrp777@gmail.com>
2022-11-28 07:48:22 +0100

    C Route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnb():
-    """web application must be listening on 0.0.0.0, port 5000"""
-    return "Hello HBNB!"
+def index():
+    ''' Function that display Hello HBNB! '''
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug=True)  # host="0.0.0.0"
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,23 @@
+
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application:"""
+''' script that starts a Flask web application '''
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnb():
-    """web application must be listening on 0.0.0.0, port 5000"""
-    return "Hello HBNB!"
+def index():
+    ''' Function that display Hello HBNB! '''
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb1():
-    """print HBNB"""
-    return "HBNB"
+def hbnb():
+    ''' Function that display HBNB '''
+    return 'HBNB'
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug=True)  # host="0.0.0.0"
+    app.run(host='0.0.0.0', port='5000')
