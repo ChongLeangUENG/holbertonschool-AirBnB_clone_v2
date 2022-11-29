4435d629532e498adddccc5011a6e77719ee395a
ChongLeangUENG <leangcnrp777@gmail.com>
2022-11-29 13:20:12 +0100

    States list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,9 +18,10 @@ def teardown_db(exception):
 @app.route('/states_list', strict_slashes=False)
 def states():
     ''' Function that return list of all states '''
-    all_states = storage.all(State).values()
+    all_states = storage.all(State)
     return render_template('7-states_list.html', all_states=all_states)
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port='5000')
